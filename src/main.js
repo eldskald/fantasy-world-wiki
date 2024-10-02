d120add1e3979a0e77f1412fb6e98154c2e1c1a3
eldskald <rafaelbordoni00@gmail.com>
2024-10-02 02:54:02 -0300

    Implement the table of contents component
    A custom HTML element to use on articles. It scans its parent element
    for `h2`s, `h3`, `h4`s, `h5`s and `h6`s elements and lists links for
    them.

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import { detectArticle, toArticle } from "./navigation/articles.js";
 import { setAnchors } from "./components/anchor.js";
+import { TableOfContents } from "./components/table-of-contents.js";
+
+// This is to add the custom HTML elements to the document.
+customElements.define("table-of-contents", TableOfContents);
 
 // This is to allow HTML documents to call `toArticle`, since this is a module
 // type script we can't just do things like `onclick="toArticle('article')"`
 // because the script is deferred, so we put these functions on window where
-// they will be accessible to buttons
+// they will be accessible to buttons.
 window.toArticle = toArticle;
 
-// These final lines are just the loading when the page is loaded the first time
+// These final lines are just the loading when the page loads the first time.
 detectArticle();
 setAnchors();
