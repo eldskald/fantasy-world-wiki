5a50b9b9ba4ca6a5563b5c7bcdec518aa0062503
eldskald <rafaelbordoni00@gmail.com>
2024-10-10 03:15:24 -0300

    Improve theme switcher themes
    Tests if clicking it works in both themes.

--- tests/light-mode.test.js
+++ tests/light-mode.test.js
@@ -1,13 +1,16 @@
 import { beforeEach, describe, expect, test } from "@jest/globals";
 import { initDom } from "./utils/init-dom.js";
 
-describe("light mode on load", () => {
+describe("light mode", () => {
     beforeEach(async () => {
         localStorage.theme = "light";
         await initDom();
     });
 
-    test("should not style body with dark", () => {
+    test("should not style body with dark and switch mode when clicked", () => {
         expect(document.body.classList.contains("dark")).toBe(false);
+        document.getElementById("theme-switcher").click();
+        expect(document.body.classList.contains("dark")).toBe(true);
+        expect(localStorage.theme).toBe("dark");
     });
 });
