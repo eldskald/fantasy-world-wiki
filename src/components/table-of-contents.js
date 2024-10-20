4b8a0abf6b679bae0e8f14a2f47272b3d654a77f
eldskald <rafaelbordoni00@gmail.com>
2024-10-20 01:15:13 -0300

    Update table of contents
    We shouldn't link to h5 and h6 elements, we should have at least one
    option of minor header to subsect text without having it added to the
    table of contents, so we're using h5 and h6 for this.

--- src/components/table-of-contents.js
+++ src/components/table-of-contents.js
@@ -4,8 +4,7 @@ export class TableOfContents extends HTMLElement {
     }
 
     connectedCallback() {
-        const headers =
-            this.parentElement.querySelectorAll("h2, h3, h4, h5, h6");
+        const headers = this.parentElement.querySelectorAll("h2, h3, h4");
         const tocHeader = document.createElement("h3");
         tocHeader.innerHTML = window.imports.settings.labels.tableOfContents;
         this.appendChild(tocHeader);
@@ -22,10 +21,6 @@ export class TableOfContents extends HTMLElement {
                 a.className = "font-sans ml-4";
             } else if (header.tagName === "H4") {
                 a.className = "font-sans ml-8";
-            } else if (header.tagName === "H5") {
-                a.className = "font-sans ml-12";
-            } else if (header.tagName === "H6") {
-                a.className = "font-sans ml-16";
             }
             li.appendChild(a);
             list.appendChild(li);
