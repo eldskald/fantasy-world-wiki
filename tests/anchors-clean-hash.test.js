c4585421aed84e4974c51d3afcfd3b4d3686cefc
eldskald <rafaelbordoni00@gmail.com>
2024-10-21 02:03:11 -0300

    Fix anchors not erasing URL hash
    If you clicked a link on a table of contents, it would set a hash on the
    current location. Then, if you clicked a link to another article, map or
    the close location link, it would do its job but the hash would remain
    there.
    
    This commit fixes this behaviour by making all links always clean
    hashes.

---
+++ tests/anchors-clean-hash.test.js
@@ -0,0 +1,21 @@
+import { beforeEach, describe, expect, test } from "@jest/globals";
+import { moddedArticles } from "./mocks/articles.js";
+import { initDom } from "./utils/init-dom.js";
+import { isArticleLoaded } from "./utils/is-article-loaded.js";
+
+describe("anchors to close article", () => {
+    beforeEach(async () => {
+        const a1 = document.createElement("a");
+        a1.href = "http://localhost/?article=article1#hash";
+        const a2 = document.createElement("a");
+        a2.setAttribute("toarticle", "article2");
+        await initDom([a1, a2]);
+        a1.click();
+        a2.click();
+    });
+
+    test("should go to article and erase URL hash", () => {
+        expect(isArticleLoaded("article2", moddedArticles.article2)).toBe(true);
+        expect(new URL(document.location.href).hash).toBe("");
+    });
+});
